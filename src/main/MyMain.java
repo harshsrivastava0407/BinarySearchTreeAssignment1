ca3b65343ace7d2573e3d72fa042bfb309b9b507
Harsh Srivastave <harsh.srivastava_cs.aiml19@gla.ac.in>
2021-04-13 23:31:59 -0700

    main completed

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -4,5 +4,17 @@ public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
         // and perform insert, search, traversal, delete
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(60);
+        tree.insert(10);
+        tree.insert(70);
+        tree.insert(25);
+        tree.insert(49);
+        tree.insert(42);
+        tree.inOrder(tree.getRoot());
+        tree.insert(30);
+        tree.inOrder(tree.getRoot());
+        System.out.println(tree.search(49));
+        System.out.println(tree.search(52));
     }
 }
